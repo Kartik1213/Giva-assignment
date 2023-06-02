25e569d049c069594582ba19de749e7b144101c3
Kartik Wadhwa <kartik.w17@iiits.in>
2023-06-02 20:24:06 +0530

    CONH

--- src/App.js
+++ src/App.js
@@ -2,52 +2,51 @@ import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import MovieSearch from './components/MovieSearch';
+import MovieHeading from './components/MovieHeading';
+import SearchBox from './components/SearchBox';
 
 const App = () => {
-	const [movies, setMovies] = useState([
-        {
-            "Title": "Star Wars: Episode IV - A New Hope",
-            "Year": "1977",
-            "imdbID": "tt0076759",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode V - The Empire Strikes Back",
-            "Year": "1980",
-            "imdbID": "tt0080684",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-        },
-        {
-            "Title": "Star Wars: Episode VI - Return of the Jedi",
-            "Year": "1983",
-            "imdbID": "tt0086190",
-            "Type": "movie",
-            "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
-        }
-    ]);
 
-  //   const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  
 
-	// const getMovieRequest = async () => {
-	// 	const url = `http://www.omdbapi.com/?s=star wars&apikey=263d22d8`;
+	const getMovieRequest = async (searchValue) => {
+    console.log(searchValue)
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZTUwOTQxOGNiOTIxY2Y2NjZmZGY4NDBhYTY3YjkzYyIsInN1YiI6IjY0NzlmMjk5ZTMyM2YzMDBlNTIzZGU4YyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.qtPRyrrrSkQRjRYYdlYFeDTl57J248_qcDU9Hm1bYgc'
+      }
+    };
+    if(searchValue == ''){
+      var url = 'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1'
+    }
+    else{
+      var url = `https://api.themoviedb.org/3/search/movie?query=${searchValue}&include_adult=false&language=en-US&page=1`
+    }
+		const response = await fetch(url, options);
+		const responseJson = await response.json();
 
-	// 	const response = await fetch(url);
-	// 	const responseJson = await response.json();
+    console.log(responseJson)
 
-	// 	if (responseJson.Search) {
-	// 		setMovies(responseJson.Search);
-	// 	}
-	// };
+		if (responseJson.results) {
+			setMovies(responseJson.results);
+		}
+	};
 
-	// useEffect(() => {
-	// 	getMovieRequest();
-	// }, []);
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
 
 
 	return (
 		<div className='container-fluid movie-app'>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
 			<div className='row'>
         <MovieSearch movies={movies} />
 			</div>
--- src/components/MovieSearch.js
+++ src/components/MovieSearch.js
@@ -4,8 +4,12 @@ const MovieSearch = (props) => {
 	return (
 		<>
 			{props.movies.map((movie, index) => (
-				<div className='image-container d-flex justify-content-start m-3'>
-					<img src={movie.Poster} alt='movie'></img>
+				<div className='image-container d-flex justify-content-start align-items-center m-3'>
+					<img src={'https://image.tmdb.org/t/p/w500' + movie.poster_path} alt='movie' height="300" width="200"></img>
+                    <div className='overlay d-flex align-items-center justify-content-center'>
+						Add to Favourites
+					</div>
+                    <h2>{movie.original_title}</h2>
 				</div>
 			))}
 		</>
